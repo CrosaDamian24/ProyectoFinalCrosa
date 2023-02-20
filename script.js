d81963f9436019888b5b4f1e39a0b8ba8eab82d0
Crosa Damián <dcrosa@prevencionart.com.ar>
2023-02-19 22:49:55 -0300

    subo febrero

--- script.js
+++ script.js
@@ -1,129 +1,350 @@
-validaEdad();
-
-function validaEdad() {
-  let edad = prompt("Bienvenido/a. Ingresa tu edad");
-
-  if (edad < 18) {
-    alert("Debes solicitar a un mayor de 18 años que realice la compra");
-  } else {
-    let opcion = Number(busquedaTipo());
-
-    busquedaIndividuales(opcion);
+//postresnombre
+class Postres {
+  constructor(id, nombre, precio, descripcion) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.descripcion = descripcion;
+  }
+  sumaIva(precio) {
+    return (precio = precio * 1.21);
   }
 }
+const postreInd = [
+  new Postres(
+    1,
+    "Oreo",
+    800,
+    "Una capa de Oreo, dulce de leche y crema chantillí."
+  ),
+  new Postres(
+    2,
+    "Chocolina",
+    750,
+    "Una capa de Chocolinas, dulce de leche y crema chantillí."
+  ),
+  new Postres(
+    3,
+    "Gula",
+    850,
+    "Una capa de Chocolinas y Oreo, dulce de leche y crema chantillí."
+  ),
+  new Postres(
+    4,
+    "Cheesecake",
+    750,
+    "Capa de Vanillas recubiertas de queso crema y futos rojos"
+  ),
+];
 
-function busquedaTipo() {
-  return Number(
-    prompt(
-      "Seleccione una de las siguientes opciones \n 1- Postres individuales (25% desc. en la segunda unidad comprando cantidades pares) \n 2- Tortas "
-    )
-  );
-}
+const div = document.getElementById("div");
+// const carrito = [];
+const iconoCarro = document.getElementById("carrito")
+iconoCarro.disabled = true
 
-//Postres individuales
-function busquedaIndividuales(opcion) {
-  if ((opcion > 0) & (opcion <= 2)) {
-    switch (opcion) {
-      case 1:
-        let opcion = Number(
-          prompt(
-            "1- Oreo $800 \n2- Chocolina $750\n3- Gula $850\n4- Cheesecake $750"
-          )
-        );
-        cantidades(opcion);
-        break;
+const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+let sumaTotalCarro = 0;
+let conEnvio = 0;
+let conDesEfectivo = 0;
+const comprar = document.getElementById("comprar");
+comprar.disabled = true;
 
-      case 2:
-        alert("Estamos trabajando para pronto ofrecerte estos productos");
-        busquedaIndividuales(busquedaTipo());
-        break;
+fetch("/data.json")
+  .then((res) => res.json())
+  .then(async (data) => {
+    await accionAsincrona();
+    data.forEach((postre) => {
+      console.log(data);
+      let postreRenderizado = document.createElement("div");
+      postreRenderizado.className = "col-xl-3";
+
+      postreRenderizado.innerHTML = `
+    <div  class="card"  >
+          <div class="card-body">
+            <h5 class="card-title">Postre ${postre.nombre}</h5>
+            <img src=${postre.img} alt="" class="img-fluid">
+            <p class="card-text">${postre.descripcion}</p>
+                <span>Precio: <strong>$ ${
+                  postre.precio
+                }</strong> (Sin IVA)</span>
+                <br>
+                <span>Cantidad:</span>
+                <strong>
+                <output id= ${
+                  postre.nombre
+                } type="number" readonly ></output></strong>
+                <br>
+
+            <button class="btn btn-primary" id = ${postre.id}>+</button>
+            <button class="btn btn-danger" disabled id = ${
+              postre.nombre + postre.id
+            }>-</button>
+        </div>
+    </div>
+    </div>
+    `;
+      document.getElementById("spinner").style.display = "none";
+      div.append(postreRenderizado);
+    });
+    postreInd.forEach((postre) => {
+      const boton = document.getElementById(postre.id);
+      boton.addEventListener("click", () => añadePostre(postre));
+
+      const restaProducto = document.getElementById(postre.nombre + postre.id);
+      restaProducto.addEventListener("click", () =>
+        quitaPostre(postre.nombre, postre.sumaIva(postre.precio))
+      );
+    });
+    actualizaTotalCarrito();
+  })
+  .catch((err) => console.log(err));
+
+const accionAsincrona = async () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
+};
+
+// postreInd.forEach((postre) => {
+
+//   let postreRenderizado = document.createElement("div");
+//   postreRenderizado.className = "col-xl-3";
+
+//   postreRenderizado.innerHTML = `
+//     <div  class="card"  >
+//           <div id= ${postre.nombre} class="card-body">
+//             <h5 class="card-title">Postre ${postre.nombre}</h5>
+//             <p class="card-text">${postre.descripcion}</p>
+//                 <span>Precio: $ ${postre.precio}</span>
+//                 <br>
+//                 <p>Cantidad</p>
+
+//             <button class="btn btn-primary" id = ${postre.id}>+</button>
+//             <button class="btn btn-danger" disabled id = ${
+//               postre.nombre + postre.id
+//             }>-</button>
+//         </div>
+//     </div>
+//     </div>
+//     `;
+//   div.append(postreRenderizado);
+
+//   const boton = document.getElementById(postre.id);
+//   boton.addEventListener("click", () => añadePostre(postre));
+
+//   const restaProducto = document.getElementById(postre.nombre + postre.id);
+//   restaProducto.addEventListener("click", () =>
+//     quitaPostre(postre.nombre, postre.sumaIva(postre.precio))
+//   );
+// });
+
+// actualizaTotalCarrito()
+
+// AGREGO POSTRE AL CARRITO
+const añadePostre = (postre) => {
+  let productoExiste = carrito.find((item) => item.id === postre.id);
+
+  const resta = document.getElementById(postre.nombre + postre.id);
+
+  resta.disabled = false;
+
+  if (productoExiste === undefined) {
+    carrito.push({
+      id: postre.id,
+      nombre: postre.nombre,
+      precio: postre.sumaIva(postre.precio),
+      cantidad: 1,
+    });
+  } else {
+    productoExiste.cantidad = productoExiste.cantidad + 1;
+    productoExiste.precio =
+      productoExiste.precio + postre.sumaIva(postre.precio);
+  }
+  iconoCarro.disabled = false
+  subirCarritoLocal();
+  actualizaTotalCarrito();
+
+  Toastify({
+    text: "Añadido al carrito",
+
+    duration: 3000,
+  }).showToast();
+};
+
+// QUITO POSTRE DEL CARRITO
+const quitaPostre = (nombre, precio) => {
+  for (const iterator of carrito) {
+    if (iterator.nombre === nombre) {
+      iterator.cantidad = iterator.cantidad - 1;
+      iterator.precio = iterator.precio - precio;
+
+      const cantidad = document.getElementById(nombre);
+
+      cantidad.innerHTML = iterator.cantidad;
+      cantidad.style.disabled = true;
+      if (iterator.cantidad === 0) {
+        cantidad.disabled = false;
+      }
+      if (iterator.cantidad === 0) {
+        const indice = carrito.findIndex((elemento, indice) => {
+          if (elemento.nombre === nombre) {
+            return true;
+          }
+        });
+        carrito.splice(indice, 1);
+
+        const restaProducto = document.getElementById(nombre + iterator.id);
+
+        //  restaProducto.remove()
+        restaProducto.disabled = true;
+        cantidad.value = "";
+        iconoCarro.disabled = true
+      }
     }
-  } else {
-    alert("Debe Seleccionar una opcion correcta");
-    // ValidaEdad();
-    busquedaIndividuales(busquedaTipo());
+    carrito.precio = iterator.precio;
+    actualizaTotalCarrito();
   }
+  subirCarritoLocal();
+  actualizaTotalCarrito();
+  Toastify({
+    text: "Quitado del carrito",
+    style: {
+      background: "#b00000",
+    },
+    duration: 3000,
+  }).showToast();
+};
+
+function actualizaTotalCarrito() {
+  let sumaTot = 0;
+  let tot = 0;
+  comprar.disabled = true;
+  for (const cantida of carrito) {
+    tot = tot + cantida.cantidad;
+    sumaTot = sumaTot + cantida.precio;
+    const restaProducto = document.getElementById(cantida.nombre + cantida.id);
+    const cantidad = document.getElementById(cantida.nombre);
+    cantidad.innerHTML = cantida.cantidad;
+    restaProducto.disabled = false;
+    comprar.disabled = false;
+    iconoCarro.disabled = false
+  }
+  
+  sumaTotalCarro = sumaTot;
+  document.getElementById("totcarro").value = tot;
 }
 
-function cantidades(tipo) {
-  if ((tipo > 0) & (tipo <= 4)) {
-    let cantidad = Number(prompt("Ingrese la cantidad"));
-    let par = esPar(cantidad);
-    let i = 0;
-    let preciofin = 0;
-    let acum = 0;
+// selector de envio
+const envio = document.getElementById("envio");
+const precioEnvio = document.getElementById("precio")
 
-    switch (par) {
-      case false:
-        if (tipo == 1) {
-          let preciofinal = total(800, cantidad);
-          alert("Su pedido es " + cantidad + " Oreo por $ " + preciofinal);
-        } else if (tipo == 2) {
-          let preciofinal = total(750, cantidad);
-          alert("Su pedido es " + cantidad + " Chocolina por $ " + preciofinal);
-        } else if (tipo == 3) {
-          let preciofinal = total(850, cantidad);
-          alert("Su pedido es " + cantidad + " Gula por $ " + preciofinal);
-        } else if (tipo == 4) {
-          let preciofinal = total(750, cantidad);
-          alert(
-            "Su pedido es " + cantidad + " Cheesecake por $ " + preciofinal
-          );
-        } else {
-          ("Ninguna opcion es correcta");
-        }
-        break;
-      case true:
-        if (tipo == 1) {
-          for (1; i < cantidad; i += 2) {
-            acum = acum += 2;
-            preciofin = (800 + Number(descuento(800))) * (cantidad / 2);
-          }
-          alert("Su pedido es " + cantidad + " Oreo por $" + preciofin);
-          //
-        } else if (tipo == 2) {
-          for (1; i < cantidad; i += 2) {
-            acum = acum += 2;
-            preciofin = (750 + Number(descuento(750))) * (cantidad / 2);
-          }
-          alert("Su pedido es " + cantidad + " Chocolina por $" + preciofin);
-        } else if (tipo == 3) {
-          for (1; i < cantidad; i += 2) {
-            acum = acum += 2;
-            preciofin = (850 + Number(descuento(850))) * (cantidad / 2);
-          }
-          alert("Su pedido es " + cantidad + " Gula por $" + preciofin);
-        } else if (tipo == 4) {
-          for (1; i < cantidad; i += 2) {
-            acum = acum += 2;
-            preciofin = (750 + Number(descuento(750))) * (cantidad / 2);
-          }
-          alert("Su pedido es " + cantidad + " Cheesecake por $" + preciofin);
-        } else {
-          ("Ninguna opcion es correcta");
-        }
-        break;
+envio.addEventListener("change", () => {
+  if (envio.value === "SI") {
+    precioEnvio.value = " $500";
+    conEnvio = 1;
+  } else {
+    precioEnvio.value = "";
+    conEnvio = 0;
+  }
+});
 
-      default:
-        break;
+// selector de medio de pago
+const medioPago = document.getElementById("medioPago");
+const descuentoPago = document.getElementById("descuento")
+
+medioPago.addEventListener("change", () => {
+  if (medioPago.value === "EFECTIVO") {
+    descuentoPago.value = " 5 % de descuento";
+    conDesEfectivo = 1;
+  } else {
+    descuentoPago.value = "";
+    conDesEfectivo = 0;
+  }
+});
+
+// boton de compra
+  comprar.addEventListener("click", () =>
+  calculosCompraFin(conEnvio, conDesEfectivo, sumaTotalCarro)
+);
+
+
+
+const calculosCompraFin = (conEnvio, conDesEfectivo, sumaTotalCarro) => {
+  let precioTotal = 0;
+  let mensaje = "";
+
+  precioTotal =
+    conDesEfectivo === 1
+      ? sumaTotalCarro - sumaTotalCarro * 0.05
+      : sumaTotalCarro;
+
+  // if (conDesEfectivo === 1) {
+  //   precioTotal = sumaTotalCarro - sumaTotalCarro * 0.05;
+  // } else {
+  //   precioTotal = sumaTotalCarro;
+  // }
+  if (precioTotal !== 0) {
+    if (conEnvio === 1) {
+      precioTotal = precioTotal + 500;
+      mensaje =
+        "En 1 hora recibirá su pedido\n Total: $" + precioTotal.toFixed(2);
+    } else {
+      mensaje =
+        " Puede pasar a retir su pedido por nuestro local en 30 minutos\n Total: $" +
+        precioTotal.toFixed(2);
     }
-  } else {
-    alert("Debe seleccionar una opcion correcta");
-    busquedaIndividuales(1);
+
+    comprar.disabled = true;
+    iconoCarro.disabled = true
+
+    swal({
+      title: "Compra realizada con éxito!",
+      text: mensaje,
+      icon: "success",
+      button: "OK",
+    });
+
+    for (const carro of carrito) {
+      const resta = document.getElementById(carro.nombre + carro.id);
+      const cantidad = document.getElementById(carro.nombre)
+      cantidad.value = ""
+
+      resta.disabled = true;
+    }
+    carrito.splice(0, carrito.length);
+    subirCarritoLocal();
+    actualizaTotalCarrito();
   }
+
+};
+
+//boton carrito de compras
+const carro = document.getElementById("carrito");
+carro.addEventListener("click", muestraCarrito);
+
+function muestraCarrito() {
+  let total = 0;
+  let padre = document.getElementById("modalcarrito");
+  padre.innerHTML = "";
+
+  const carro = JSON.parse(localStorage.getItem("carrito"));
+
+  carro.forEach((carro) => {
+    total = carrito.reduce((acc, el) => acc + el.precio, 0);
+    console.log(carro.nombre);
+
+    const agregaProductos = document.createElement("div");
+    agregaProductos.innerHTML = ` <h4> ${carro.cantidad} x ${carro.nombre} <strong>$${carro.precio}</strong> </h4>`;
+    padre.appendChild(agregaProductos);
+  });
+
+  const totalCarro = document.createElement("div");
+  totalCarro.innerHTML = ` <h3>Total <strong>$ ${total}</strong> </h3>`;
+  padre.appendChild(totalCarro);
 }
 
-//descuento en la segunda unidad
-function descuento(costo) {
-  return Number(costo - costo * 0.25);
-}
-
-//saber si es par o no el numero de cantidad seleccionado
-function esPar(numero) {
-  return numero % 2 == 0;
-}
-
-//Calculo de precio para cantidades impares
-function total(precio, cant) {
-  return precio * cant;
-}
+const subirCarritoLocal = () => {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
